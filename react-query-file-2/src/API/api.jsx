ede9d0e691c5430028c48a9d63c9125c5fb94b75
100rbhsarin <saurabh007630@gmail.com>
2025-01-16 22:07:23 +0530

    "#" just a part time update

---
+++ react-query-file-2/src/API/api.jsx
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+});
+
+// to fetch the data
+export const fetchPosts = async (pageNumber) => {
+    try {
+      const res = await api.get(`/posts?_start=${pageNumber}&_limit=3`);
+      return res.status === 200 ? res.data : [];
+    } catch (error) {
+      console.log(error);
+    }
+  };
--- react-query-file-2/src/Pages/FetchRQ.jsx
+++ react-query-file-2/src/Pages/FetchRQ.jsx
@@ -1,10 +1,57 @@
+import {
+    keepPreviousData,
+    useMutation,
+    useQuery,
+    useQueryClient,
+  } from "@tanstack/react-query";
+  import { deletePost, fetchPosts, updatePost } from "../API/api";
+  import { NavLink } from "react-router-dom";
+  import { useState } from "react";
 
 
 export const FetchRQ = ()=>{
 
+    const queryClient = useQueryClient();
+
+    const { data, isPending, isError, error } = useQuery({
+      queryKey: ["posts", pageNumber], // useState
+      queryFn: () => fetchPosts(pageNumber), // useEffect
+      placeholderData: keepPreviousData,
+    });
+  
+    if (isPending) return <p>Loading...</p>;
+    if (isError) return <p> Error: {error.message || "Something went wrong!"}</p>;
     return(
         <>
-    <h1>FetchRQ</h1>
+  <div>
+      <ul className="section-accordion">
+        {data?.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <NavLink to={`/rq/${id}`}>
+                <p>{id}</p>
+                <p>{title}</p>
+                <p>{body}</p>
+              </NavLink>
+              <button onClick={() => deleteMutation.mutate(id)}>Delete</button>
+       
+            </li>
+          );
+        })}
+      </ul>
+
+      <div className="pagination-section container">
+        <button
+          disabled={pageNumber === 0 ? true : false}
+          onClick={() => setPageNumber((prev) => prev - 3)}
+        >
+          Prev
+        </button>
+        <p>{pageNumber / 3 + 1}</p>
+        <button onClick={() => setPageNumber((prev) => prev + 3)}>Next</button>
+      </div>
+    </div>
         </>
     )
  
